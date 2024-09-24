0322642726e057decfc2a1acb909c9a28b241909
Kate <134648812+rudenkoekatiae@users.noreply.github.com>
2024-09-24 19:51:04 +0300

    Update lab_2_1.py

--- lab_2_1.py
+++ lab_2_1.py
@@ -1,19 +1,21 @@
 import math
 from math import factorial
-
+a = 0
+b = 21
+h = 2
 def func(x):
     total = 0
     n = 1
     m = 2 * n
+    d  = 0.001
     while True:
         f = (x ** m - 1) / factorial(m - 1)
-        if (abs(f) < 0.001):
+        if (abs(f) < d):
             break
         total += f
         m += 2
-    return total
-    
-for i in range(0, 21, 2):
+    return total    
+for i in range(a, b, h):
     x = i / 100
     y = func(x)
     print(f"x={x:.2f}, f(x)={y:.5f}")    
