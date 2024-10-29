1a5623ac9807448565998409bde6c317ebc96f21
Kate <134648812+rudenkoekatiae@users.noreply.github.com>
2024-10-29 10:06:52 +0200

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,142 @@
+from enum import Enum
+from collections import Counter
+
+class Gender(Enum):
+    FEMALE = "Female"
+    MALE = "Male"
+    NON_BINARY = "Non-binary"
+
+class AlcoholType(Enum):
+    NON_ALCOHOLIC = 0
+    LOW_ALCOHOLIC = 1
+    ALCOHOLIC = 2
+    STRONG_ALCOHOLIC = 3
+
+owners_of_lucky_numbers = []
+
+class Guest:
+    def __init__(self, id = 0, name = "", age = 0, city = "", phone_number = "", gender = Gender, disliked_drinks = None, drink_limits = None):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.city = city
+        self.phone_number = phone_number
+        self.gender = gender
+        self.disliked_drinks = disliked_drinks
+        self.drink_limits =drink_limits or {
+            AlcoholType.NON_ALCOHOLIC: 1,
+            AlcoholType.LOW_ALCOHOLIC: 0,
+            AlcoholType.ALCOHOLIC: 0,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+
+    def is_lucky_phone_number(self):
+        seven_count = self.phone_number.count("7")
+        if seven_count > 3:
+            owners_of_lucky_numbers.append(self.name)
+            return True
+        return False
+    
+    def filter_drinks(self, drinks):
+        filtered_drinks = [drink for drink in drinks if drink.name not in self.disliked_drinks]
+        return sorted(filtered_drinks, key = lambda d: d.profit(), reverse = True)
+    
+    def get_drink_recommendations(self, drinks, cocktails):
+        drink_recommendations = []
+        cocktail_recommendations = []
+        drink_counter = Counter()
+
+        for drink in self.filter_drinks(drinks):
+            if drink_counter[drink.alcohol_type()]< self.drink_limits[drink.alcohol_type()]:
+                drink_recommendations.append(drink)
+                drink_counter[drink.alcohol_type()] += 1
+
+        for cocktail in sorted(cocktails, key = lambda c: c.profit(), reverse=True):
+            if any (drink.name in self.disliked_drinks for drink in cocktail.basic_drinks):
+                continue
+            cocktail_type = cocktail.alcohol_type()
+            if drink_counter[cocktail_type] < self.drink_limits[cocktail_type]:
+                cocktail_recommendations.append(cocktail)
+                drink_counter[cocktail_type] += 1
+
+        return drink_recommendations, cocktail_recommendations
+    
+class Party:
+    def __init__(self, day, reason):
+        self.day = day
+        self.reason = reason
+        self.guests = []
+
+    def add_guests(self, guest):
+        self.guests.append(guest)
+
+    def find_average_age(self, gender):
+        guests_ = [guest.age for guest in self.guests if guest.gender == gender]
+        if guests_:
+            return sum(guests_)/len(guests_)
+        return None
+    
+    def sort_guests(self):
+        self.guests.sort(key = lambda guest: guest.id)
+
+    def __repr__(self):
+        guest_names = ", ".join([guest.name for guest in self.guests])
+        return f"Party on {self.day} for {self.reason} with guests: {guest_names}"
+    
+class BasicDrink:
+    def __init__(self, name = "", cost = 0.0, price = 0.0, volume = 0.0, alcohol_percentage = 0.0):
+        self.name = name
+        self.cost = cost
+        self.price = price
+        self.volume = volume
+        self.alcohol_percentage = alcohol_percentage
+
+    def alcohol_type(self):
+        if self.alcohol_percentage == 0:
+            return AlcoholType.NON_ALCOHOLIC
+        elif 0 < self.alcohol_percentage < 12:
+            return AlcoholType.LOW_ALCOHOLIC
+        elif 12 <= self.alcohol_percentage < 30:
+            return AlcoholType.ALCOHOLIC
+        else:
+            return AlcoholType.STRONG_ALCOHOLIC
+        
+    def profit(self):
+        return self.price - self.cost
+    
+class Additional:
+    def __init__(self, name = '', cost= 0.0):
+        self.name = name
+        self.cost = cost
+
+class Cocktail:
+    def __init__(self, name = '', price = 0.0):
+        self.name = name
+        self.price = price
+        self.basic_drinks = {}
+        self.additionals = []
+
+    def add_basic_drinks(self, drink, volume):
+        self.basic_drinks[drink] = volume
+
+    def add_additionals(self, additional):
+        self.additionals.append(additional)       
+
+    def alcohol_type(self):
+        total_volume = sum(self.basic_drinks.values())
+        weighted_alcohol_content = sum(drink.alcohol_percentage * volume for drink, volume in self.basic_drinks.items()if isinstance(drink, BasicDrink))
+        alcohol_content =  weighted_alcohol_content / total_volume if total_volume else 0
+        if alcohol_content == 0:
+            return AlcoholType.NON_ALCOHOLIC
+        elif 0 < alcohol_content < 13:
+            return AlcoholType.LOW_ALCOHOLIC
+        elif 13 <= alcohol_content <30:
+            return AlcoholType.ALCOHOLIC
+        else:
+            return AlcoholType.STRONG_ALCOHOLIC
+        
+    def profit(self):
+        drink_cost = sum(drink.cost * volume for drink, volume in self.basic_drinks.items())
+        additional_cost = sum(additional.cost for additional in self.additionals)
+        total_cost = drink_cost + additional_cost
+        return self.price - total_cost    
---
+++ main.py
@@ -0,0 +1,493 @@
+from classes import Gender
+from classes import AlcoholType
+from classes import Guest
+from classes import Party
+from classes import owners_of_lucky_numbers
+from classes import BasicDrink
+from classes import Additional
+from classes import Cocktail
+from collections import Counter
+
+def main():
+    coke = BasicDrink("Coke", 60, 150, 0.3, 0)
+    water = BasicDrink("Water", 20, 50, 0.3, 0)
+    lemonade = BasicDrink("Lemonade", 70, 150, 0.3, 0)
+    tonic_water = BasicDrink("Tonic water", 70, 155, 0.3, 0)
+    sprite = BasicDrink("Sprite", 70, 150, 0.3, 0)
+    bourbon = BasicDrink("Bourbon", 800, 1500, 0.2, 40)
+    rum = BasicDrink("Rum", 450, 850, 0.2, 35)
+    vodka = BasicDrink("Vodka", 400, 720, 0.1, 40)
+    jagermeister = BasicDrink("Jagermeister", 900, 2000, 0.25, 35)
+    aperol = BasicDrink("Aperol", 540, 1300,0.3, 11)
+    whiskey = BasicDrink("Whiskey", 860, 1750, 0.2, 40)
+    baileys = BasicDrink("Baileys", 990, 1990, 0.3, 17)
+    cognac = BasicDrink("Cognac", 920, 1720, 0.2, 40)
+    sparkle_water = BasicDrink("Sparkle water", 25, 55, 0.3, 0)
+    apple_juice = BasicDrink("Apple juice", 80, 200, 0.3, 0)
+    orange_juice = BasicDrink("Orange juice", 80, 200, 0.3, 0)
+    peach_juice = BasicDrink("Peach juice", 80, 200, 0.3, 0)
+    grape_juice = BasicDrink("Grape juice", 80, 200, 0.3, 0)
+    tequila = BasicDrink("Tequila", 770, 1500, 0.2, 37)
+    gin = BasicDrink("Gin", 650, 1300, 0.2, 39)
+    red_wine = BasicDrink("Red wine", 490, 760, 0.3, 16)
+    rose_wine = BasicDrink("Rose wine", 490, 760, 0.3, 14)
+    white_wine = BasicDrink("White wine", 470, 760, 0.3, 16)
+    rose_sparkling_wine = BasicDrink("Rose sparkling wine", 480, 760, 0.3, 12)
+    white_sparkling_wine = BasicDrink("White sparkling wine", 490, 760, 0.3, 14)
+    beer = BasicDrink("Beer", 120, 300, 0.7, 7)
+    zhyvchyk = BasicDrink("Zhyvchyk", 40, 90, 0.3, 0)
+
+    lemon = Additional("Lemon", 5)
+    ice = Additional("Ice", 5)
+    orange = Additional("Orange", 5)
+    caramel_syrup = Additional("Caramel syrup", 5)
+    mint = Additional("Mint", 5)
+    cinnamon = Additional("Cinnamon", 3)
+    coconut = Additional("Coconut", 3)
+    cucumber = Additional("Cucumber", 3)
+    cocktails_cherry = Additional("Cocktails cherry", 15)
+    lemon_fresh = Additional("Lemon fresh", 6)
+    espresso = Additional("Espresso", 10)
+
+    vodka_tonic = Cocktail("Vodka Tonic", 135)
+    vodka_tonic.add_basic_drinks(vodka, 0.05)
+    vodka_tonic.add_basic_drinks(tonic_water, 0.2)
+    vodka_tonic.add_additionals(lemon_fresh)
+    vodka_tonic.add_additionals(ice)
+    
+    devils_cave = Cocktail("Devil`s cave", 200)
+    devils_cave.add_basic_drinks(vodka, 0.3)
+    devils_cave.add_additionals(lemon_fresh)
+    devils_cave.add_additionals(ice)
+    devils_cave.add_additionals(mint)
+
+    mermaid_tears = Cocktail("Mermaid tears", 190)
+    mermaid_tears.add_basic_drinks(tequila, 0.2)
+    mermaid_tears.add_basic_drinks(grape_juice, 0.2)
+    mermaid_tears.add_additionals(coconut)
+    mermaid_tears.add_additionals(ice)
+
+    spring_garden = Cocktail("Spring garden", 210)
+    spring_garden.add_basic_drinks(white_wine, 0.18)
+    spring_garden.add_basic_drinks(grape_juice, 0.2)
+    spring_garden.add_basic_drinks(apple_juice, 0.1)
+    spring_garden.add_additionals(ice)
+
+    bloody_cherry = Cocktail("Bloody cherry", 230)
+    bloody_cherry.add_basic_drinks(red_wine, 0.25)
+    bloody_cherry.add_additionals(espresso)
+    bloody_cherry.add_additionals(cocktails_cherry)
+    bloody_cherry.add_additionals(ice)
+
+    mellow_breeze = Cocktail("Mellow Breeze", 180)
+    mellow_breeze.add_basic_drinks(white_wine, 0.2)
+    mellow_breeze.add_basic_drinks(lemonade, 0.15)
+    mellow_breeze.add_basic_drinks(peach_juice, 0.15)
+    mellow_breeze.add_additionals(ice)
+    mellow_breeze.add_additionals(mint)
+
+    spiced_tonic = Cocktail("Spiced Tonic", 220)
+    spiced_tonic.add_basic_drinks(gin, 0.2)
+    spiced_tonic.add_basic_drinks(tonic_water, 0.15)
+    spiced_tonic.add_basic_drinks(lemon_fresh, 0.1)
+    spiced_tonic.add_additionals(cinnamon)
+    spiced_tonic.add_additionals(ice)
+
+    sunny_rush = Cocktail("Sunny Rush", 200)
+    sunny_rush.add_basic_drinks(vodka, 0.15)
+    sunny_rush.add_basic_drinks(orange_juice, 0.2)
+    sunny_rush.add_basic_drinks(sprite, 0.1)
+    sunny_rush.add_additionals(orange)
+    sunny_rush.add_additionals(caramel_syrup)
+
+    chill_bliss = Cocktail("Chill Bliss", 160)
+    chill_bliss.add_basic_drinks(aperol, 0.2)
+    chill_bliss.add_basic_drinks(white_sparkling_wine, 0.25)
+    chill_bliss.add_basic_drinks(sparkle_water, 0.1)
+    chill_bliss.add_additionals(lemon)
+    chill_bliss.add_additionals(mint)
+
+    tropical_twist = Cocktail("Tropical Twist", 240)
+    tropical_twist.add_basic_drinks(rum, 0.2)
+    tropical_twist.add_basic_drinks(coke, 0.15)
+    tropical_twist.add_basic_drinks(coconut, 0.05)
+    tropical_twist.add_additionals(lemon)
+    tropical_twist.add_additionals(ice)
+
+    tropical_punch = Cocktail("Tropical Punch", 250)
+    tropical_punch.add_basic_drinks(whiskey, 0.15)
+    tropical_punch.add_basic_drinks(orange_juice, 0.2)
+    tropical_punch.add_basic_drinks(peach_juice, 0.1)
+    tropical_punch.add_additionals(cinnamon)
+    tropical_punch.add_additionals(ice)
+
+    berry_twist = Cocktail("Berry Twist", 210)
+    berry_twist.add_basic_drinks(cognac, 0.15)
+    berry_twist.add_basic_drinks(rose_wine, 0.2)
+    berry_twist.add_basic_drinks(grape_juice, 0.1)
+    berry_twist.add_additionals(cocktails_cherry)
+    berry_twist.add_additionals(ice)
+
+    espresso_bliss = Cocktail("Espresso Bliss", 230)
+    espresso_bliss.add_basic_drinks(jagermeister, 0.2)
+    espresso_bliss.add_basic_drinks(vodka, 0.1)
+    espresso_bliss.add_additionals(espresso)
+    espresso_bliss.add_additionals(caramel_syrup)
+    espresso_bliss.add_additionals(ice)
+
+    citrus_burst = Cocktail("Citrus Burst", 190)
+    citrus_burst.add_basic_drinks(tequila, 0.15)
+    citrus_burst.add_basic_drinks(lemonade, 0.2)
+    citrus_burst.add_basic_drinks(lemon_fresh, 0.05)
+    citrus_burst.add_additionals(lemon)
+    citrus_burst.add_additionals(ice)
+
+    island_dream = Cocktail("Island Dream", 250)
+    island_dream.add_basic_drinks(bourbon, 0.15)
+    island_dream.add_basic_drinks(sprite, 0.15)
+    island_dream.add_basic_drinks(coconut, 0.05)
+    island_dream.add_additionals(orange)
+    island_dream.add_additionals(mint)
+
+    creamy_harmony = Cocktail("Creamy Harmony", 200)
+    creamy_harmony.add_basic_drinks(baileys, 0.2)
+    creamy_harmony.add_basic_drinks(rum, 0.1)
+    creamy_harmony.add_additionals(cinnamon)
+    creamy_harmony.add_additionals(caramel_syrup)
+    creamy_harmony.add_additionals(ice)
+
+    refreshing_splash = Cocktail("Refreshing Splash", 140)
+    refreshing_splash.add_basic_drinks(coke, 0.2)
+    refreshing_splash.add_basic_drinks(lemonade, 0.2)
+    refreshing_splash.add_additionals(ice)
+    refreshing_splash.add_additionals(lemon)
+
+    morning_boost = Cocktail("Morning Boost", 150)
+    morning_boost.add_basic_drinks(zhyvchyk, 0.25)
+    morning_boost.add_basic_drinks(peach_juice, 0.15)
+    morning_boost.add_additionals(cucumber)
+    morning_boost.add_additionals(mint)
+
+    citrus_joy = Cocktail("Citrus Joy", 160)
+    citrus_joy.add_basic_drinks(orange_juice, 0.25)
+    citrus_joy.add_basic_drinks(tonic_water, 0.2)
+    citrus_joy.add_additionals(ice)
+    citrus_joy.add_additionals(lemon)
+
+    berry_fizz = Cocktail("Berry Fizz", 160)
+    berry_fizz.add_basic_drinks(grape_juice, 0.2)
+    berry_fizz.add_basic_drinks(rose_sparkling_wine, 0.2)
+    berry_fizz.add_additionals(cocktails_cherry)
+    berry_fizz.add_additionals(mint)
+
+    minty_green = Cocktail("Minty Green", 140)
+    minty_green.add_basic_drinks(water, 0.2)
+    minty_green.add_basic_drinks(sparkle_water, 0.2)
+    minty_green.add_additionals(lemon_fresh)
+    minty_green.add_additionals(mint)
+    minty_green.add_additionals(ice)
+
+    apple_beer_mix = Cocktail("Apple Beer Mix", 10)
+    apple_beer_mix.add_basic_drinks(apple_juice, 0.2)
+    apple_beer_mix.add_basic_drinks(beer, 0.3)
+
+    playfulness = Cocktail("Playfulness", 185)
+    playfulness.add_basic_drinks(jagermeister, 0.2)
+    playfulness.add_basic_drinks(zhyvchyk, 0.2)
+    playfulness.add_additionals(ice)
+    playfulness.add_additionals(mint)
+    playfulness.add_additionals(lemon_fresh)
+
+
+
+    katia = Guest(
+        id=0, name="Katia", age=21, city="Lviv", phone_number="2102180507", gender=Gender.FEMALE,
+        disliked_drinks=["Beer", "Cognac", "Bourbon"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 2,
+            AlcoholType.LOW_ALCOHOLIC: 3,
+            AlcoholType.ALCOHOLIC: 1,
+            AlcoholType.STRONG_ALCOHOLIC: 3
+        }
+    )
+    victoria = Guest(
+        id=1, name="Victoria", age=19, city="Lviv", phone_number="12347777", gender=Gender.FEMALE,
+        disliked_drinks=["Beer", "Tequila", "Apple juice"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 3,
+            AlcoholType.LOW_ALCOHOLIC: 3,
+            AlcoholType.ALCOHOLIC: 2,
+            AlcoholType.STRONG_ALCOHOLIC: 2
+        }
+    )
+    andriy = Guest(
+        id=2, name="Andriy", age=20, city="Kyiv", phone_number="12345678", gender=Gender.MALE,
+        disliked_drinks=["Vodka", "Rum"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 5,
+            AlcoholType.LOW_ALCOHOLIC: 4,
+            AlcoholType.ALCOHOLIC: 2,
+            AlcoholType.STRONG_ALCOHOLIC: 2
+        }
+    )
+    adam = Guest(
+        id=3, name="Adam", age=20, city="NYC", phone_number="0258631468", gender=Gender.MALE,
+        disliked_drinks=["Vodka", "Rum"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 5,
+            AlcoholType.LOW_ALCOHOLIC: 4,
+            AlcoholType.ALCOHOLIC: 2,
+            AlcoholType.STRONG_ALCOHOLIC: 2
+        }
+    )  
+
+    jung_ho = Guest(
+        id=4, name="Jung Ho", age=18, city="Seoul", phone_number="6158631452", gender=Gender.NON_BINARY,
+        disliked_drinks=["Zhyvchyk", "Rum"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 7,
+            AlcoholType.LOW_ALCOHOLIC: 3,
+            AlcoholType.ALCOHOLIC: 1,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+    )
+
+    sophia = Guest(
+        4, "Sophia", 16, "NYC", "9877765420", Gender.FEMALE,
+        disliked_drinks=["Coke", "Lemonade",],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 8,
+            AlcoholType.LOW_ALCOHOLIC: 0,
+            AlcoholType.ALCOHOLIC: 0,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+    )
+    
+    charlie = Guest(
+        6, "Charlie", 20, "Kyiv", "1111111111", Gender.NON_BINARY,
+        disliked_drinks=["Jagermeister", "Aperol", "Whiskey"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 2,
+            AlcoholType.LOW_ALCOHOLIC: 2,
+            AlcoholType.ALCOHOLIC: 2,
+            AlcoholType.STRONG_ALCOHOLIC: 1
+        }
+    )
+    alex = Guest(
+        7, "Alex", 26, "Florence", "7771112102", Gender.NON_BINARY,
+        disliked_drinks=["Peach juice", "Rose wine"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 4,
+            AlcoholType.LOW_ALCOHOLIC: 2,
+            AlcoholType.ALCOHOLIC: 3,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+    )
+    hae_soo = Guest(
+        8, "Hae Soo", 17, "Seoul", "9568451230", Gender.MALE, 
+        disliked_drinks=["Orange juice", "Sparkling water"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 5,
+            AlcoholType.LOW_ALCOHOLIC: 0,
+            AlcoholType.ALCOHOLIC: 0,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+    )  
+
+    stacy = Guest(
+        9, "Stacy", 15, "Paris", "1237890456", Gender.FEMALE,
+        disliked_drinks=["Zhyvchyk", "Peach juice"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 5,
+            AlcoholType.LOW_ALCOHOLIC: 0,
+            AlcoholType.ALCOHOLIC: 0,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+    )
+
+    elizabeth = Guest(
+        10, "Elizabeth", 19, "London", "7771112102", Gender.FEMALE,
+        disliked_drinks=["Red wine", "Rose sparkling wine", "White sparkling wine"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 2,
+            AlcoholType.LOW_ALCOHOLIC: 2,
+            AlcoholType.ALCOHOLIC: 1,
+            AlcoholType.STRONG_ALCOHOLIC: 2
+        }
+    )
+    victoria = Guest(
+        id=1, name="Victoria", age=19, city="Lviv", phone_number="12347777", gender=Gender.FEMALE,
+        disliked_drinks=["Beer", "Tequila", "Apple juice"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 3,
+            AlcoholType.LOW_ALCOHOLIC: 3,
+            AlcoholType.ALCOHOLIC: 2,
+            AlcoholType.STRONG_ALCOHOLIC: 2
+        }
+    )
+    ava = Guest(
+        11, "Ava", 15, "Paris", "1020304050", Gender.FEMALE,
+        disliked_drinks=["Water", "Sprite"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 9,
+            AlcoholType.LOW_ALCOHOLIC: 0,
+            AlcoholType.ALCOHOLIC: 0,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+    )
+    henry = Guest(
+        12, "Henry", 17, "Lviv", "8070904060", Gender.MALE,
+        disliked_drinks=["Sprite", "Orange juice"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 3,
+            AlcoholType.LOW_ALCOHOLIC: 0,
+            AlcoholType.ALCOHOLIC: 0,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+    )  
+
+    riley = Guest(
+        13, "Riley", 16, "Lviv", "1222222222", Gender.NON_BINARY,
+        disliked_drinks=["Zhyvchyk", "Tonic water", "Lemonade"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 7,
+            AlcoholType.LOW_ALCOHOLIC: 0,
+            AlcoholType.ALCOHOLIC: 0,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+    )
+
+    will = Guest(
+        14, "Will", 27, "Madrid", "8888552233", Gender.MALE,
+        disliked_drinks=["Coke", "Vodka",],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 2,
+            AlcoholType.LOW_ALCOHOLIC: 2,
+            AlcoholType.ALCOHOLIC: 2,
+            AlcoholType.STRONG_ALCOHOLIC: 2
+        }
+    )
+    
+    olivia = Guest(
+        15, "Olivia", 24, "London", "1063788573", Gender.FEMALE,
+        disliked_drinks=["Jagermeister", "Aperol", "Whiskey"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 2,
+            AlcoholType.LOW_ALCOHOLIC: 0,
+            AlcoholType.ALCOHOLIC: 2,
+            AlcoholType.STRONG_ALCOHOLIC: 3
+        }
+    )
+    mia = Guest(
+        16, "Mia", 16, "NYC", "9877765425", Gender.FEMALE,
+        disliked_drinks=["Peach juice", "Tonic water"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 4,
+            AlcoholType.LOW_ALCOHOLIC: 0,
+            AlcoholType.ALCOHOLIC: 0,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+    )
+    taylor = Guest(
+        17, "Taylor", 26, "Florence", "4567823665", Gender.NON_BINARY, 
+        disliked_drinks=["Orange juice", "Sparkling water"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 0,
+            AlcoholType.LOW_ALCOHOLIC: 1,
+            AlcoholType.ALCOHOLIC: 0,
+            AlcoholType.STRONG_ALCOHOLIC: 6
+        }
+    )  
+
+    o_jun = Guest(
+        18, "O jun", 17, "Seoul", "7373494773", Gender.NON_BINARY,
+        disliked_drinks=["Lemonade", "Peach juice"],
+        drink_limits={
+            AlcoholType.NON_ALCOHOLIC: 5,
+            AlcoholType.LOW_ALCOHOLIC: 0,
+            AlcoholType.ALCOHOLIC: 0,
+            AlcoholType.STRONG_ALCOHOLIC: 0
+        }
+    )
+
+    guests = [
+        victoria, adam, jung_ho, katia, andriy, sophia, charlie, alex, hae_soo, stacy, elizabeth, ava,
+        henry, riley, will, olivia, mia, taylor, o_jun
+    ]
+
+    drinks = [
+        coke, water, lemonade, tonic_water, sprite, bourbon, rum,
+        vodka, jagermeister, aperol, whiskey, baileys, cognac,
+        sparkle_water, apple_juice, orange_juice, peach_juice, grape_juice,
+        tequila, gin, red_wine, rose_wine, white_wine, rose_sparkling_wine, white_sparkling_wine,
+        beer, zhyvchyk
+    ]  
+    cocktails = [
+        vodka_tonic, vodka_tonic, devils_cave, mermaid_tears, spring_garden, bloody_cherry, mellow_breeze,
+        spiced_tonic, sunny_rush, chill_bliss, tropical_twist, tropical_punch, berry_twist,
+        espresso_bliss, citrus_burst, island_dream, creamy_harmony, refreshing_splash,
+        morning_boost, citrus_joy, berry_fizz, minty_green, apple_beer_mix, playfulness
+    ]
+ 
+
+
+    new_year_party = Party("31/12/2024", "New Year")
+    helloween_party_in_lviv = Party("31/10/2024", "Helloween in Lviv")
+
+    def add_adult_guests_to_party(party, guests):
+        for guest in guests:
+            if guest.age >= 18:
+                party.add_guests(guest)
+
+    def add_lviv_guests_to_party(party, guests):
+        for guest in guests:
+            if guest.city =="Lviv":
+                party.add_guests(guest)
+
+    add_adult_guests_to_party(new_year_party, guests)
+    add_lviv_guests_to_party(helloween_party_in_lviv, guests)
+
+    print(new_year_party)
+    print(helloween_party_in_lviv)
+
+    print("Average age of male guests in New Year party:", new_year_party.find_average_age(Gender.MALE))
+    print("Average age of female guests in New Year party:", new_year_party.find_average_age(Gender.FEMALE))
+    print("Average age of non-binary guests in New Year party:", new_year_party.find_average_age(Gender.NON_BINARY))
+
+    print("Average age of male guests in Lviv party:", helloween_party_in_lviv.find_average_age(Gender.MALE))
+    print("Average age of female guests in Lviv party:", helloween_party_in_lviv.find_average_age(Gender.FEMALE))
+    print("Average age of non-binary guests in Lviv party:", helloween_party_in_lviv.find_average_age(Gender.NON_BINARY))
+    
+
+    for guest in guests:
+        guest.is_lucky_phone_number()
+
+    new_year_party.sort_guests()
+    helloween_party_in_lviv.sort_guests()
+
+    print(f"Owners of lucky numbers: {owners_of_lucky_numbers}")
+
+    drink_count = Counter()
+    total_profit = 0
+
+    for guest in guests:
+        drinks_recommended, cocktails_recommended = guest.get_drink_recommendations(drinks, cocktails)
+
+        for drink in drinks_recommended:
+            drink_count[drink.name] += 1
+            total_profit += drink.profit()
+
+        for cocktail in cocktails_recommended:
+            drink_count[cocktail.name] += 1
+            total_profit += cocktail.price - sum(d.cost for d in cocktail.basic_drinks)
+
+        print(f"Recommendations for {guest.name}:")
+        print("  Drinks:", [drink.name for drink in drinks_recommended])
+        print("  Cocktails:", [cocktail.name for cocktail in cocktails_recommended])
+        print()
+
+    print("Total drinks needed for parties:", dict(drink_count))
+    print("Total profit from parties:", total_profit)
+
+if __name__ == '__main__':
+    main()
